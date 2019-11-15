764532af89817d413043c305567d3bb192f94893
Dimitris Chloptsios <dchlop15@gmail.com>
2019-11-15 15:32:39 +0200

    Added job title

--- jobs/models.py
+++ jobs/models.py
@@ -6,5 +6,6 @@
 from django.db import models
 
 class Job(models.Model):
+    title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
